f431e7769d644d8fa9cc78f24d181329ea8ca1c4
tamaratsymbaliuk <tsymbalt14@gmail.com>
2022-12-29 20:15:34 -0600

    Row actions in Datatable

--- Aura Framework/force-app/main/default/aura/CTRecentChanges/CTRecentChangesController.js
+++ Aura Framework/force-app/main/default/aura/CTRecentChanges/CTRecentChangesController.js
@@ -1,5 +1,7 @@
 ({
     doInit : function(component, event, helper) {
+        const scope = component.get("v.scope");
+        if(scope === "person"){
       component.set('v.columns', [
             {label: 'Name', fieldName: 'Name', type: 'text'},
             {label: 'Phone ', fieldName: 'Mobile__c', type: 'text'},
@@ -7,6 +9,31 @@
             {label: 'Status Update Date', fieldName: 'Status_Update_Date__c', type: 'date'},
             {label: 'Token', fieldName: 'Token__c', type: 'text'},
         ]);
-     helper.fetchRecentHealthChanges(component);
+     
+    }else{
+        component.set('v.columns', [
+            {label: 'Name', fieldName: 'Name', type: 'text'},
+            {label: 'Status', fieldName: 'Status__c', type: 'text'},
+            {label: 'Red Score', fieldName: 'Red_Score__c', type: 'number'},
+            {label: 'Pincode', fieldName: 'Pincode__c', type: 'text'},
+            {label: 'Address', fieldName: 'Address__c', type: 'text'},
+            {label: 'Status Update Date', fieldName: 'Status_Update_Date__c', type: 'date'},
+        ]);
     }
+    helper.fetchRecentHealthChanges(component);
+},
+
+handleKeyUp : function(component, event, helper) {
+    const isEnterKey = event.keyCode === 13;
+        var queryTerm = component.find('enter-search').get('v.value');
+        if(!queryTerm){
+            component.set("v.data", component.get("v.initialResponse"));
+        }
+        if (isEnterKey) {
+            component.set("v.issearching", true);
+            helper.searchRecords(component, queryTerm);
+        }
+},
+
+
 });
--- Aura Framework/force-app/main/default/aura/CTRecentChanges/CTRecentChangesHelper.js
+++ Aura Framework/force-app/main/default/aura/CTRecentChanges/CTRecentChangesHelper.js
@@ -1,6 +1,6 @@
 ({
     fetchRecentHealthChanges : function(component) {
-      let action = component.get("c.getRecentPersonHealthChanges");
+      let action = component.get("v.scope") === "person" ? component.get("c.getRecentPersonHealthChanges") : component.get("c.getRecentLocationHealthChanges");
 
       action.setCallback(this, function(response){
           const state = response.getState();
@@ -8,9 +8,31 @@
           if(state === 'SUCCESS'){
               const data = response.getReturnValue();
               component.set("v.data", data);
+              component.set("v.initialResponse", date);
           }
 
       });
       $A.enqueueAction(action);
-    }
+    },
+
+     searchRecords : function(component, queryTerm) {
+        let action = component.get("v.scope") === "person" ? component.get("c.searchPeople") : component.get("c.searchLocation");
+        action.setParams({
+             "searchTerm": queryTerm
+        });
+        action.setCallback(this, function(response){
+            const state = response.getState();
+
+          if(state === 'SUCCESS'){
+              const data = response.getReturnValue();
+           
+              if(data && data.length > 0){
+                  component.set("v.data", data);
+              }
+              component.set("v.issearching", false);
+          }
+
+        });
+        $A.enqueueAction(action);
+     }
 })
